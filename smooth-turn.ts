fe7e7ea0c3eb03a4ab398f4ab31777784de9008a
felixtsu <humaeks@gmail.com>
2020-12-24 15:36:49 +0800

    Update pxt.json, smooth-turn.ts

--- smooth-turn.ts
+++ smooth-turn.ts
@@ -10,9 +10,12 @@ namespace smoothturn{
 
     //%block="is $sprite close enough to tile center"
     //%blockid=iscloseenoughtotilecenter 
-    export function isCloseEnoughToTileCenter(sprite:Sprite):boolean {
+    export function isCloseEnoughToTileCenter(sprite:Sprite, epsilon?:number):boolean {
+        if(epsilon == NaN) {
+            epsilon = EPSILON
+        }
         let loc = tiles.locationOfSprite(sprite)    
-        return distanceOf(sprite, loc) <= EPSILON;
+        return distanceOf(sprite, loc) <= epsilon;
     }
 
     //%block="is location in $direction of $sprite wall tile"
@@ -27,5 +30,46 @@ namespace smoothturn{
         sprite.x = loc.x
         sprite.y = loc.y
     }
+    
+    let _movingSprite:Sprite = null
+
+    export function moveSprite(sprite:Sprite, vx:number, vy:number) {
+        controller.moveSprite(sprite, vy, vy)
+        _movingSprite = sprite
+    }
+
+    game.onUpdate(function(){
+        if (_movingSprite != null) {
+            if (controller.up.isPressed()) {
+                if (isCloseEnoughToTileCenter(_movingSprite, 4) && !isWall(_movingSprite, CollisionDirection.Top) 
+                    && _movingSprite.isHittingTile(CollisionDirection.Top)) {
+                    alignToTileCenter(_movingSprite)
+                    _movingSprite.y -= 1 //pushing in
+                }
+            }
+            if (controller.right.isPressed()) {
+                if (isCloseEnoughToTileCenter(_movingSprite, 4) && !isWall(_movingSprite, CollisionDirection.Right) 
+                    && _movingSprite.isHittingTile(CollisionDirection.Right)) {
+                    alignToTileCenter(_movingSprite)
+                    _movingSprite.x += 1 //pushing in
+                }
+            }
+            if (controller.down.isPressed()) {
+                if (isCloseEnoughToTileCenter(_movingSprite, 4) && !isWall(_movingSprite, CollisionDirection.Bottom) 
+                    && _movingSprite.isHittingTile(CollisionDirection.Bottom)) {
+                    alignToTileCenter(_movingSprite)
+                    _movingSprite.y += 1 //pushing in
+                }
+            }
+            if (controller.left.isPressed()) {
+                if (isCloseEnoughToTileCenter(_movingSprite, 4) && !isWall(_movingSprite, CollisionDirection.Left) 
+                    && _movingSprite.isHittingTile(CollisionDirection.Left)) {
+                    alignToTileCenter(_movingSprite)
+                    _movingSprite.x -= 1 //pushing in
+                }
+            }
+        }
+    })
+
 
 }
